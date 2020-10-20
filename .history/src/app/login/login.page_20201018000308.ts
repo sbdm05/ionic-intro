c8dd3a2788dd57d2b20c07c0587971c201f19899
sbdm05 <s.baibou@gmail.com>
2020-10-20 15:47:28 +0200

    first commit

---
+++ .history/src/app/login/login.page_20201018000308.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { Router} from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+  // private variable of router of the type Router
+  constructor(
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+  }
+
+  onLogin(){
+    this.router.navigate(['/home']);
+  }
+
+}
---
+++ src/app/login/login.page.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { Router} from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+  // private variable of router of the type Router
+  constructor(
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+  }
+
+  onLogin(){
+    console.log('test')
+    this.router.navigate(['/home']);
+  }
+
+}
+
+
